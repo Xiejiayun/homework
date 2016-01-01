26c8a2fb403dea8696102d63e9c458f3d4269353
xie <furnace09@gmail.com>
2016-01-02 00:41:40 +0800

    new examples about java concurrent

--- base/src/nju/software/concurrent/DeadLockExample.java
+++ base/src/nju/software/concurrent/DeadLockExample.java
@@ -7,30 +7,40 @@ package nju.software.concurrent;
 public class DeadLockExample {
 
     private static DeadLockExample one = new DeadLockExample();
-
     private static DeadLockExample two = new DeadLockExample();
 
     public static void main(String[] args) {
-        Thread first = new Thread();
-        Thread second = new Thread();
-
-    }
-
-    public void operationOne() {
-        synchronized (one) {
-            System.out.println("operation one step 1 finished");
-            synchronized (two) {
-                System.out.println("operation one step 2 finished");
+        Thread first = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (one) {
+                    System.out.println("operation one step 1 finished");
+                    synchronized (two) {
+                        System.out.println("operation one step 2 finished");
+                    }
+                }
             }
-        }
+        });
+        Thread second = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (two) {
+                    System.out.println("operation two step 1 finished");
+                    synchronized (one) {
+                        System.out.println("operation two step 2 finished");
+                    }
+                }
+            }
+        });
+
+        first.start();
+        second.start();
+
     }
 
-    public void operationTwo() {
-        synchronized (two) {
-            System.out.println("operation two step 1 finished");
-            synchronized (one) {
-                System.out.println("operation two step 2 finished");
-            }
-        }
-    }
 }
--- base/src/nju/software/concurrent/ThreadPoolExample.java
+++ base/src/nju/software/concurrent/ThreadPoolExample.java
@@ -1,7 +1,13 @@
 package nju.software.concurrent;
 
 /**
+ * A simple example using Thread Pool to manager threads
  * Created by Xie on 2015/12/31.
  */
 public class ThreadPoolExample {
+
+    public static void main(String[] args) {
+
+    }
+
 }
